92ef9db8fd3b81fc320e39354c5cbc045d3d2fcc
Kumar Harsh <kharsh39@gmail.com>
2023-08-08 03:29:23 +0530

    Add custom instrumentation

--- calculator/views.py
+++ calculator/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+import scout_apm.api
 
 # Create your views here.
 def calculator(request):
     print("hello")
     expr = str(request.GET.get('expression'))
 
+    scout_apm.api.WebTransaction.start("Expression Evaluation")
     ans = eval(expr)
+    scout_apm.api.WebTransaction.stop()
 
     print(ans)
 
