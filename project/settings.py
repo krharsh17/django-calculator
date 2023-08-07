3237038bc8c900554506adcc431b1a816ebfa047
Kumar Harsh <kharsh39@gmail.com>
2023-08-08 02:48:18 +0530

    Add Scout APM

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'calculator'
+    'calculator',
+    'scout_apm.django',
 ]
 
 MIDDLEWARE = [
