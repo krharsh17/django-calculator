5f5cb2a0c1a06a3059cce05219e1dd3cd8a7ada6
Kumar Harsh <kharsh39@gmail.com>
2023-08-08 20:44:05 +0530

    Update views.py

--- calculator/views.py
+++ calculator/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-import scout_apm.api
 import os
 
 # Create your views here.
 def calculator(request):
     print("hello")
     expr = str(request.GET.get('expression'))
-
-    config = {
-        "name": os.environ['SCOUT_NAME'],
-        "key": os.environ['SCOUT_KEY'],
-        "monitor": os.environ['SCOUT_MONITOR'],
-    }
-
-    print(os.environ['SCOUT_NAME'])
-
-    scout_apm.api.install(config=config)
-
-    scout_apm.api.WebTransaction.start("Expression Evaluation")
     ans = eval(expr)
-    scout_apm.api.WebTransaction.stop()
-
-    print(ans)
 
     return render(request, 'calculator.html', {'ans': ans})
